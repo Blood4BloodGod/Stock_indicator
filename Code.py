4e21b790902245bd9f55722d51e99096da77c5e7
MingTai Han <mingtai@ucsb.edu>
2026-04-18 23:48:02 -0700

    Add Code.py to repository

---
+++ Code.py
@@ -0,0 +1,8 @@
+#Aim to use kaggle's given data to prject the future price of  using a model
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+
+
+
